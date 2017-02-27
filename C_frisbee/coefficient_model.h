40b23a904a00d37857167fa4dff6eae88a307c8d
Tom McClintock <tmcclintock@email.arizona.edu>
2017-02-27 16:01:35 -0700

    Started working on the C stuff. Have the coefficient model implemented.

---
+++ C_frisbee/coefficient_model.h
@@ -0,0 +1,5 @@
+double C_lift(double alpha, double PL0, double PLa);
+double C_drag(double alpha, double PD0, double PDa, double alpha0);
+double C_x(double wx, double wz, double PTxwx, double PTxwz);
+double C_y(double alpha, double wy, double PTy0, double PTywy, double PTya);
+double C_z(double wz, double PTzwz);
