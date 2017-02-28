855cf361336632fd3b5ad49309c677606b67728d
Tom McClintock <tmcclintock@email.arizona.edu>
2017-02-28 11:53:42 -0700

    Working on the interface file that can be called in the python code.

---
+++ C_frisbee/C_frisbee.py
@@ -0,0 +1,41 @@
+"""
+This is a python wrapper for the c_frisbee.so shared library, which can be compiled in this directory from the C code that is present.
+"""
+
+import numpy as np
+import ctypes, os, sys, inspect
+from ctypes import c_double, c_int, POINTER, cdll
+
+#This is to allow this to work from anywhere, in case we implement a setup.py
+library_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+"/c_frisbee.so"
+cflib = cdll.LoadLibrary(library_path)
+
+def get_trajectory(initial_positions,flight_time,N_times,params):
+    """Get the trajectory from the driver.
+
+    Args:
+        initial_positions (array_like): All initial positions and velocities of the disc.
+        flight_time (float): Total flight time; seconds.
+        N_times (float): The number of times to evaluate the positions at.
+        params (array_like): List of force/torque parameters.
+
+    Returns:
+        times (array_like): List of all times that the positions are evaluated at.
+        trajectory (array_like): 2d array of dimensions 12 by N_times that has all coordinates of the disc over the course of the trajectory.
+
+    """
+    #Outline the argument types
+    driver = cflib.driver
+    driver.argtypes = [POINTER(c_double),POINTER(c_double),c_double,c_int,POINTER(c_double)]
+
+    #Create an array for the trajectory+time
+
+    #Create pointers for the input arrays
+    initial_positions_in = initial_positions.ctypes.data_as_(POINTER(c_double))
+    params_in = params.ctypes.data_as_(POINTER(c_double))
+
+
+    #in progress
+    
+    #Return an array of all of the positions at all the times
+    return
