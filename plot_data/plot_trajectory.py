61159e459a4bba571fac3eb1bd44f1531af646f0
ehannah <ehannah@email.arizona.edu>
2017-04-02 16:57:16 -0700

    Created program to plot trajectories, stored as arrays in text files.

---
+++ plot_data/plot_trajectory.py
@@ -0,0 +1,27 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.animation as animation
+
+def update_line(num, data, line):
+    line.set_data(data[..., :num])
+    return line,
+
+trajectory=np.loadtxt("simulated_trajectory_300FPS,mod1.txt")
+
+x = trajectory[:,0]
+y = trajectory[:,1]
+z = trajectory[:,2]
+
+zeros = np.zeros(len(x))
+
+fig = plt.figure()
+ax = fig.add_subplot(111,projection='3d')
+
+line = ax.plot(x, y ,z)
+shadow_1 = ax.plot(zeros, y, z)
+shadow_2 = ax.plot(x, zeros, z)
+
+#plt.plot(x,y,z)
+plt.show()
+plt.clf()
