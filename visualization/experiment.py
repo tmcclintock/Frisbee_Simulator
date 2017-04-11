08b191f6493663769578ab00e5c510c6dfb8d574
Tom McClintock <tmcclintock@email.arizona.edu>
2017-04-11 16:51:58 -0700

    Made the multiple plot visualization prettier. got the angles to display really well in the polar plots like in an airplane console.

--- visualization/experiment.py
+++ visualization/experiment.py
@@ -12,19 +12,22 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 from matplotlib import gridspec
+plt.rc('text',usetex=True)
 
 def update_lines(num, dataLines, lines):
-    """
-    #template for lines:
     for i in range(len(lines)):
-    line = lines[i]
-    data = dataLines[i]
-    switch(i):
-    case 0: 3D line
-    case 1: polar plot
-    case 2: 2D line
+        line = lines[i]
+        data = dataLines[i]
+        if i < 3: #Trajectory plot
+            line.set_data(data[0:2, :num])
+            line.set_3d_properties(data[2, :num])
+        elif 2 < i <5: #Angle plots
+            line.set_data([data[0][num],data[0][num]+np.pi],[1,1])
+        elif i == 5: #Spin vs time
+            line.set_data(data[0:2, :num])
+
     return lines
-    """
+
 
     #Each dataLine is a 3xnum array
     #lines contains the matplotlib line objects
@@ -44,10 +47,10 @@ print trajectory.shape
 trajectory = trajectory[::4] #Reduce the size for the gif purposes
 
 times,x,y,z = trajectory.T[:4]
-print trajectory.shape
 phi, theta, gamma = trajectory.T[7:10]
+phi *= -1 #This is to orient things correctly for the plot
+theta *= -1 #This is to orient things correctly for the plot
 phid, thetad, gammad = trajectory.T[10:]
-print gamma[0]
 
 zeros = np.zeros(len(x))
 
@@ -58,28 +61,39 @@ gs.update(left=0.05, right=0.78, wspace=0.55)
 
 #These are the axes and lines for the 3D plot
 ax0 = plt.subplot(gs[:, :2], projection='3d')
-ax0.set_title('Trajectory')
+ax0.set_title(r'${\rm Trajectory}$')
 ax0.set_zlim(0,max(z))
 line = ax0.plot(x, y ,z)[0]
 wall_shadow   = ax0.plot(x, zeros+max(y), z, linestyle='--')[0]
 ground_shadow = ax0.plot(x, y, zeros, linestyle='--')[0]
+ax0.set_xlabel(r"${\rm X\ [m] }$")
+ax0.set_ylabel(r"${\rm Y\ [m] }$")
+ax0.set_zlabel(r"${\rm Z\ [m] }$")
 
 #While the 3D plot contains the trajectory,
 #these extra plots can contain the pitch and roll angles, updated
 #along with the trajectory
 ax1 = plt.subplot(gs[0,2], projection='polar')
+philine = ax1.plot([phi[0],phi[0]+np.pi], [1,1])[0]
 ax1.set_rmax(1)
 ax1.set_rticks([])
-philine = ax1.plot(phi,times/max(times))[0]
+ax1.set_title(r"${\rm Left\ Right}$")
+ax1.set_xticklabels([])
+
 ax2 = plt.subplot(gs[1,2], projection='polar')
+thetaline = ax2.plot([theta[0],theta[0]+np.pi],[1,1])[0]
 ax2.set_rmax(1)
 ax2.set_rticks([])
-thetaline = ax2.plot(theta,times/max(times))[0]
+ax2.set_xticklabels([])
+ax2.set_title(r"${\rm Front\ Back}$")
+
 ax3 = plt.subplot(gs[2,2])
 gammadline = ax3.plot(times, gammad)[0]
+ax3.set_ylabel(r"${\rm Spin}$ $[rad/s]$")
+ax3.set_xlabel(r"${\rm Time}$ $[s]$")
 
 
-dataLines = [[x, y, z], [x, zeros+max(y), z], [x, y, zeros], [phi, times/max(times)], [theta, times/max(times)], [times, gammad]]
+dataLines = [[x, y, z], [x, zeros+max(y), z], [x, y, zeros], [phi], [theta], [times, gammad]]
 dataLines = [np.array(DL) for DL in dataLines] #Gotta convert to numpy arrays
 lines = [line, wall_shadow, ground_shadow, philine, thetaline, gammadline]
 print len(x)," frames in the animation"
