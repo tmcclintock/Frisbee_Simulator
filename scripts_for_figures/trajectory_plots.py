75a63e0bf9bbc8c82e50251698b6ee181d83766a
Tom McClintock <tmcclintock89@gmail.com>
2017-05-14 16:08:26 -0700

    Added the 3D option for the trajectory plots.

--- scripts_for_figures/trajectory_plots.py
+++ scripts_for_figures/trajectory_plots.py
@@ -3,6 +3,7 @@ Create figure 1, which shows the trajectory. Create this both with and without s
 """
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 plt.rc("text", usetex=True, fontsize=24)
 plt.rc("errorbar", capsize=3)
 
@@ -12,13 +13,31 @@ xs = x + np.random.randn(len(x))*0.05
 ys = y + np.random.randn(len(y))*0.05
 zs = z + np.random.randn(len(z))*0.05
 
-#original throw was integrated at 300 fps, or dt = 1/300.
-#downsampling by 10 mean we have a 30 fps throw, which is realistic for most cameras
-ds = 10
-plt.plot(x, z)
-plt.plot(xs[::ds], zs[::ds], zorder=-1)
-plt.xlabel(r"$x$ displacement $[{\rm m}]$")
-plt.ylabel(r"$z$ displacement $[{\rm m}]$")
-plt.subplots_adjust(bottom=0.17, left=0.15)
-plt.gcf().savefig("figures/xz_scatter.pdf")
-plt.show()
+
+
+def traj_2D():
+    ds = 10
+    plt.plot(x, z, c='b')
+    plt.plot(xs[::ds], zs[::ds], zorder=-1, c='k')
+    plt.xlabel(r"$x\ {\rm displacement}\ [{\rm m}]$")
+    plt.ylabel(r"$z\ {\rm displacement}\ [{\rm m}]$")
+    plt.subplots_adjust(bottom=0.17, left=0.15)
+    plt.gcf().savefig("figures/xz_scatter.pdf")
+    plt.show()
+
+def traj_3D():
+    ds = 10
+    plt.rc("text", usetex=True, fontsize=16)
+    fig = plt.figure()
+    ax = fig.add_subplot(111,projection='3d')
+    plt.plot(x, y, z, c='b')
+    plt.plot(xs[::ds], ys[::ds], zs[::ds], alpha=0.5, c='k')
+    plt.xlabel(r"$x\ {\rm displacement}\ [{\rm m}]$")
+    plt.ylabel(r"$y\ {\rm displacement}\ [{\rm m}]$")
+    ax.set_zlabel(r"$z\ {\rm displacement}\ [{\rm m}]$")
+    plt.gcf().savefig("figures/3d_scatter.pdf")
+    plt.show()
+
+if __name__ == "__main__":
+    traj_2D()
+    traj_3D()
