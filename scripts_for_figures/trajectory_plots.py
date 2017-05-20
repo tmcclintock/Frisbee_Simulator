fed4d8c339b9313819d13f141bb8bc6afc233bb9
Tom McClintock <tmcclintock@email.arizona.edu>
2017-05-19 17:56:22 -0700

    Made it a lot easier to create the trajectory plots with the correct FPSs.

--- scripts_for_figures/trajectory_plots.py
+++ scripts_for_figures/trajectory_plots.py
@@ -7,16 +7,22 @@ from mpl_toolkits.mplot3d import Axes3D
 plt.rc("text", usetex=True, fontsize=24)
 plt.rc("errorbar", capsize=3)
 
+#Read in the data and down-sample to get the FPS we want
 datapath = "../simulation_data/sample_throw.txt"
-t, x, y, z, xe, ye, ze = np.loadtxt(datapath, unpack=True)
+trajectory = np.loadtxt(datapath)
+T = trajectory[-1, 0] -trajectory[0, 0] #Total time
+N = len(trajectory)
+FPS = 60
+trajectory = trajectory[::int(N/(FPS*T))]
+print trajectory.shape
+t, x, y, z, xe, ye, ze = trajectory.T
 xs = x + np.random.randn(len(x))*0.05
 ys = y + np.random.randn(len(y))*0.05
 zs = z + np.random.randn(len(z))*0.05
 
 def traj_2D():
-    ds = 10
     plt.plot(x, z, c='b')
-    plt.plot(xs[::ds], zs[::ds], zorder=-1, c='k')
+    plt.plot(xs, zs, zorder=-1, c='k')
     plt.xlabel(r"$x\ {\rm displacement}\ [{\rm m}]$")
     plt.ylabel(r"$z\ {\rm displacement}\ [{\rm m}]$")
     plt.subplots_adjust(bottom=0.17, left=0.15)
@@ -24,9 +30,8 @@ def traj_2D():
     plt.show()
 
 def traj_2D_with_err():
-    ds = 10
     plt.plot(x, z, c='b')
-    plt.errorbar(xs[::ds], zs[::ds], xerr=xe[::ds], yerr=ze[::ds], zorder=-1, c='k', ls='')
+    plt.errorbar(xs, zs, xerr=xe, yerr=ze, zorder=-1, c='k', ls='')
     plt.xlabel(r"$x\ {\rm displacement}\ [{\rm m}]$")
     plt.ylabel(r"$z\ {\rm displacement}\ [{\rm m}]$")
     plt.subplots_adjust(bottom=0.17, left=0.15)
@@ -34,12 +39,11 @@ def traj_2D_with_err():
     plt.show()
 
 def traj_3D():
-    ds = 10
     plt.rc("text", usetex=True, fontsize=16)
     fig = plt.figure()
     ax = fig.add_subplot(111,projection='3d')
     plt.plot(x, y, z, c='b')
-    plt.plot(xs[::ds], ys[::ds], zs[::ds], alpha=0.5, c='k')
+    plt.plot(xs, ys, zs, alpha=0.5, c='k')
     plt.xlabel(r"$x\ {\rm displacement}\ [{\rm m}]$")
     plt.ylabel(r"$y\ {\rm displacement}\ [{\rm m}]$")
     ax.set_zlabel(r"$z\ {\rm displacement}\ [{\rm m}]$")
